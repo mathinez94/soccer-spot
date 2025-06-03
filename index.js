4d3cfa4649bec1ce7a69d4826a668dff0efd0455
mathinez94 <audumathias@gmail.com>
2025-06-03 14:46:43 +0100

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import authRouter from './routes/authRouter.js';
-import dotenv from 'dotenv';
-dotenv.config();
 
 
 const app = express();
@@ -12,5 +10,5 @@ app.use(express.json());
 app.use('/auth', authRouter); 
 
 app.listen(process.env.PORT, () => { 
-    // console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
