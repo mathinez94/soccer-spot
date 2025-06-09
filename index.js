8703dbe84b14ffa8bb2c06b0809a6af04e3df95c
mathinez94 <audumathias@gmail.com>
2025-06-09 19:53:10 +0100

    returned the auth in index file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(authRouter); 
+app.use('/auth', authRouter); 
 
 app.listen(process.env.PORT, () => { 
     console.log(`Server is running on port ${process.env.PORT}`);
