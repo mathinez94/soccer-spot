4c399a6159837bf927d9b625727e08d4c50861af
mathinez94 <audumathias@gmail.com>
2025-06-09 18:40:34 +0100

    removed the /auth from index file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/auth', authRouter); 
+app.use(authRouter); 
 
 app.listen(process.env.PORT, () => { 
     console.log(`Server is running on port ${process.env.PORT}`);
